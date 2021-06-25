9668ee9817c4a004e5609828bef8890ee03f33fc
yungshun317 <seraph317@gmail.com>
2021-06-25 14:57:49 +0800

    [FEAT] Define the `User` model class

---
+++ src/main/java/yungshun/chang/springbootticketmanagementaop/model/User.java
@@ -0,0 +1,33 @@
+package yungshun.chang.springbootticketmanagementaop.model;
+
+public class User {
+
+    private Integer userid;
+    private String username;
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public User(Integer userid, String username) {
+        this.userid = userid;
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "User [userid=" + userid + ", username=" + username + "]";
+    }
+}
