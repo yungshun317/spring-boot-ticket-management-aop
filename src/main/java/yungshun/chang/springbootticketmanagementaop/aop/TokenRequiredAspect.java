2ee3ad4d9cce5740395a2911b03bc8a335d3cfa1
yungshun317 <seraph317@gmail.com>
2021-06-25 17:41:09 +0800

    [FEAT] Use explicit annotation-based AOP

--- src/main/java/yungshun/chang/springbootticketmanagementaop/aop/TokenRequiredAspect.java
+++ src/main/java/yungshun/chang/springbootticketmanagementaop/aop/TokenRequiredAspect.java
@@ -1,8 +1,16 @@
 package yungshun.chang.springbootticketmanagementaop.aop;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.bind.DatatypeConverter;
 
 @Aspect
 @Component
@@ -13,4 +21,26 @@ public class TokenRequiredAspect {
         System.out.println("Before tokenRequiredWithExecution");
     }
 
+    @Before("@annotation(tokenRequired)")
+    public void tokenRequiredWithAnnotation(TokenRequired tokenRequired) throws Throwable {
+        System.out.println("Before tokenRequiredWithAnnotation");
+
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        HttpServletRequest request = requestAttributes.getRequest();
+        // Check for token in request header
+        String tokenInHeader = request.getHeader("token");
+        if (StringUtils.isEmpty(tokenInHeader)) {
+            throw new IllegalArgumentException("Empty token");
+        }
+        Claims claims = Jwts.parser()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(SecurityServiceImpl.secretKey))
+                .parseClaimsJwts(tokenInHeader)
+                .getBody();
+        if (claims == null || claims.getSubject() == null) {
+            throw new IllegalArgumentException("Token Error : Claim is null");
+        }
+        if (!claims.getSubject().equalsIgnoreCase("yungshun")) {
+            throw new IllegalArgumentException("Subject doesn't match in the token");
+        }
+    }
 }
