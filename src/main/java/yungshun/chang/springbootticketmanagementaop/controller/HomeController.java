b4bd12115dd48ab7090a8e3be32bd954930d0dbd
yungshun317 <seraph317@gmail.com>
2021-06-26 14:06:50 +0800

    [FEAT] Create the `generateToken` method for the endpoint creating JWT token from subject

--- src/main/java/yungshun/chang/springbootticketmanagementaop/controller/HomeController.java
+++ src/main/java/yungshun/chang/springbootticketmanagementaop/controller/HomeController.java
@@ -1,8 +1,11 @@
 package yungshun.chang.springbootticketmanagementaop.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import yungshun.chang.springbootticketmanagementaop.service.SecurityService;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -10,6 +13,9 @@ import java.util.Map;
 @RestController
 @RequestMapping("/")
 public class HomeController {
+
+    @Autowired
+    SecurityService securityService;
 
     @ResponseBody
     @RequestMapping("/test/aop/with/execution")
@@ -29,5 +35,15 @@ public class HomeController {
         return map;
     }
 
+    @ResponseBody
+    @RequestMapping("/security/generate/token")
+    public Map<String, Object> generateToken(@RequestParam(value="subject") String subject) {
 
+        String token = securityService.createToken(subject, (15 * 1000 * 60));
+
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("result", token);
+
+        return map;
+    }
 }
