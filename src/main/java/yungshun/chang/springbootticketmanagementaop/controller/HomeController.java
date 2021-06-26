d3248c0288bc2a6196a4061850c88e7c1f7cc7b0
yungshun317 <seraph317@gmail.com>
2021-06-26 14:34:54 +0800

    [CHORE] Configure logging levels in `application.properties`

--- src/main/java/yungshun/chang/springbootticketmanagementaop/controller/HomeController.java
+++ src/main/java/yungshun/chang/springbootticketmanagementaop/controller/HomeController.java
@@ -1,5 +1,7 @@
 package yungshun.chang.springbootticketmanagementaop.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +15,8 @@ import java.util.Map;
 @RestController
 @RequestMapping("/")
 public class HomeController {
+
+    private static final Logger _logger = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     SecurityService securityService;
